1a4b14e093bf2e2bd26a84f684c5f5d055def178
greiceprogram <fanicolo@gmail.com>
2024-05-22 21:47:33 -0300

    feat:organizing the code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import TeamComposition from "./components/Team";
+
+
+
+function App() {
+  return (
+    <body>
+      <TeamComposition />
+    </body>
+  );
+}
+
+export default App;
